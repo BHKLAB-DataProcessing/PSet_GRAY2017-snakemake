efc10a2f147a10a8d6a4c9e3bdbc5e2c1a83a274
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-02-13 23:55:22 -0500

    Update GRAY.R

--- GRAY.R
+++ GRAY.R
@@ -3,6 +3,8 @@ library(readxl)
 library(openxlsx)
 library(tximport)
 library(Biobase)
+library(data.table)
+library(reshape2)
 
 getGRAYP <-
   function (
@@ -314,12 +316,108 @@ z <- c(z,c(
   rnaseq_results
   )
 )
+	  
+standardizeRawDataConcRange <- function(sens.info, sens.raw){
+	unq.drugs <- unique(sens.info$drugid)
+
+	conc.m <- data.table(melt(sens.raw[,,1], as.is=TRUE))
+	conc.m[,drugid := sens.info$drugid[match(Var1, rownames(sens.info))]]
+	conc.ranges <- conc.m[,.(l = min(value, na.rm=T), r = max(value, na.rm=T)), c("drugid", "Var1")]
+	conc.ranges[,Var1 := NULL]
+	conc.ranges <- conc.ranges[,unique(.SD), drugid]	
+	# conc.ranges[,N := .N, drugid]
+	conc.ranges.disj <- conc.ranges[, {sq <- sort(unique(c(l,r))); 
+				                       l = sq[seq(1,length(sq)-1)];
+				                       r = sq[seq(2,length(sq))];
+				                       .(l=l,r=r)}, drugid]
+    ## Function below returns all consecutive ranges of ints between 1 and N
+    returnConsInts <- function(N) {
+        stopifnot(N>0)
+        unlist(sapply(seq(1,N), function(ii) return(sapply(seq(ii, N), function(jj) return(seq(ii,jj))))), recursive=FALSE)
+    }
+    rangeNoHoles <- function(indicies, lr.tbl){
+        if(length(indicies) == 1) return(TRUE)
+        sq <- seq(indicies[1], indicies[length(indicies)]-1)
+        all(lr.tbl[["l"]][sq+1] <= lr.tbl[["r"]][sq])
+    }
+    per.drug.range.indicies <- sapply(conc.ranges.disj[,.N,drugid][,N], returnConsInts)
+
+    names(per.drug.range.indicies) <- conc.ranges.disj[,unique(drugid)] ## checked this: conc.ranges.disj[,.N,drugid][,drugid] == conc.ranges.disj[,unique(drugid)]
+    
+
+    # Check if there are any holes in the chosen range combination
+    per.drug.range.indicies <- sapply(names(per.drug.range.indicies), function(drug){
+
+        lr.tbl <- conc.ranges.disj[drugid == drug]
+        per.drug.range.indicies[[drug]][sapply(per.drug.range.indicies[[drug]], rangeNoHoles, lr.tbl = lr.tbl)]
+
+        })
+    per.drug.range.indicies.2 <- sapply(names(per.drug.range.indicies), function(drug){
+
+        lr.tbl <- conc.ranges.disj[drugid == drug]
+        res <- t(sapply(per.drug.range.indicies[[drug]], function(x) return(c(lr.tbl[x[1],l], lr.tbl[x[length(x)],r]))))
+        colnames(res) <- c("l", "r")
+        res <- data.frame(res)
+        res <- cbind(drugid = drug, res)
+        }, simplify=FALSE)
+    per.drug.range.indicies.dt <- rbindlist(per.drug.range.indicies.2)
+    
+    conc.ranges <- conc.m[,.(l = min(value, na.rm=T), r = max(value, na.rm=T)), c("drugid", "Var1")]
+    setkey(conc.m, Var1)
+    conc.m <- na.omit(conc.m)
+    setkey(conc.m, drugid, Var1, value)
+    setkey(conc.ranges, drugid, l, r)
+    # tic()
+    ## NOTE:: Data.table used for maximum speed. Probably possible to do this more intelligently by 
+    ## NOTE:: being aware of which conditions overlap, but its fast enough right now as it is.
+    chosen.drug.ranges <- lapply(unq.drugs, function(drug){
+        num.points.in.range <- apply(per.drug.range.indicies.dt[drugid==drug, .(l,r)], 1, function(rng){
+            conc.m[drugid==drug][conc.ranges[drugid==drug][l<=rng["l"]][r>=rng["r"],Var1], on="Var1"][value >= rng["l"]][value <= rng["r"],.N]
+            # conc.m[drugid==drug][, Var1]
+            })
+        max.ranges <- per.drug.range.indicies.dt[drugid==drug][which(num.points.in.range==max(num.points.in.range))]
+        max.ranges[which.max(log10(r) - log10(l)), ]
+    })
+    # toc()
+    names(chosen.drug.ranges) <- sapply(chosen.drug.ranges, `[[`, "drugid")
+    removed.experiments <- unlist(lapply(unq.drugs, function(drug){
+        rng <- unlist(chosen.drug.ranges[[drug]][,.(l,r)])
+        exp.out.range <- conc.ranges[drugid==drug][l>rng["l"] | r<rng["r"],Var1]
+        return(exp.out.range)
+        }))
+
+    sens.raw[removed.experiments,,] <- NA_real_
+    conc.ranges.kept <- conc.ranges[!Var1 %in% removed.experiments]
+
+    for(drug in unq.drugs){
+        rng <- unlist(chosen.drug.ranges[[drug]][,.(l,r)])
+        myx <- conc.ranges.kept[drugid==drug,Var1]
+        doses <- sens.raw[myx, ,"Dose"]
+        which.remove <- (doses < rng["l"] | doses > rng["r"])
+        sens.raw[myx, ,"Dose"][which(which.remove,arr.ind=TRUE)] <- NA_real_
+        sens.raw[myx, ,"Viability"][which(which.remove,arr.ind=TRUE)] <- NA_real_
+
+        ## Annotate sens info with chosen range
+        sens.info[sens.info$drugid==drug,"chosen.min.range"] <- rng["l"]
+        sens.info[sens.info$drugid==drug,"chosen.max.range"] <- rng["r"]
+    }
+    sens.info$rm.by.conc.range <- FALSE
+    sens.info[removed.experiments,"rm.by.conc.range"] <- TRUE
+
+    return(list("sens.info" = sens.info, sens.raw = sens.raw))
+}
+		 
+		 
+		 
+standardize <- standardizeRawDataConcRange(sens.info = sensitivity.info, sens.raw = raw.sensitivity)
+		 
+		 
     GRAY2017 <- PharmacoSet(molecularProfiles=z,
                             name="GRAY", 
                             cell=cellineinfo, 
                             drug=druginfo, 
-                            sensitivityInfo=sensitivity.info, 
-                            sensitivityRaw=raw.sensitivity, 
+                            sensitivityInfo= standardize$sens.info, 
+                            sensitivityRaw=standardize$sens.raw, 
                             sensitivityProfiles=sensitivity.profiles, 
                             sensitivityN=NULL,
                             curationCell=curationCell, 
