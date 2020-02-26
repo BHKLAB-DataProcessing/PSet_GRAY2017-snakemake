571c080023db33a09c7575e0f83b053565c7eb42
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-02-26 11:29:22 -0500

    Update GRAY.R

--- GRAY.R
+++ GRAY.R
@@ -270,6 +270,7 @@ print(tool_path)
   }
     print(annot)
   
+  print(tdir)
   rnaseq <- summarizeRnaSeq(dir=file.path(paste0(myDirPrefix, tdir, tool_path[r])),
                             features_annotation=annot,
                             samples_annotation=rnaseq.sampleinfo,
