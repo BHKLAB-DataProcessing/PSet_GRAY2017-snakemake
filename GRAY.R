8377c2c42afd4306639395b9fef5ab4a83ef2896
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-07 14:18:52 -0400

    Update GRAY.R

--- GRAY.R
+++ GRAY.R
@@ -511,8 +511,9 @@ print("filter done")
 GRAY2017@sensitivity$profiles[noisy_out$noisy, ] <- NA
 		 
 saveRDS(GRAY2017,file="/pfs/out/GRAY_2017.rds")
-		 
+dataset <- "GRAY"		 
 #output ORCESTRA_ID and Pachyderm commit id
+write.table(dataset, file="/pfs/out/dataset.txt", row.names = F ,quote = F, sep = "\t", col.names = F)
 write.table(ORCESTRA_ID, file="/pfs/out/orcestra_id.txt", row.names = F ,quote = F, sep = "\t", col.names = F)				   
 pach_commit_id <- Sys.getenv("PACH_OUTPUT_COMMIT_ID")
 write.table(pach_commit_id, file="/pfs/out/commit_id.txt", row.names = F ,quote = F, sep = "\t", col.names = F) 
